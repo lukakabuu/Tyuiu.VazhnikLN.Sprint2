d027554d9598746ec05c4b5695d14ef2a05a551a
Лука Важник <Tyuiu.VazhnikLN@outlook.com>
2026-09-23 19:46:52 +0500

    Task1

--- Tyuiu.VazhnikLN.Sprint2.Task1.V8.Lib/DataService.cs
+++ Tyuiu.VazhnikLN.Sprint2.Task1.V8.Lib/DataService.cs
@@ -6,12 +6,12 @@ namespace Tyuiu.VazhnikLN.Sprint2.Task1.V8.Lib
         public bool[] GetLogicOperations(int a, int b, int c, int d)
         {
             bool[] res = new bool[6];
-            res[0] = (15 < 16) | (17 > 335);
-            res[1] = (15 != 17) & (16 >= 335);
-            res[2] = (15 <= 16) || (17 > 335);
-            res[3] = (15 != 16) && (17 > 335);
+            res[0] = (15 < 16) | (17 != 335);
+            res[1] = (15 >= 16) & (17 == 335);
+            res[2] = (15 <= 16) || (17 != 335);
+            res[3] = (15 >= 16) && (17 != 335);
             res[4] = !res[0];
-            res[5] = (15 < 16) ^ (17 <= 335);
+            res[5] = (15 < 16) ^ (17 != 335);
             return res;
         }
     }
