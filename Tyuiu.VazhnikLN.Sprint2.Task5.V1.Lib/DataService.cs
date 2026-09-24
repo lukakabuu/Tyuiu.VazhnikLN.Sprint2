e0383fe2ddb2760b6ae8bc1e593be89a6f2c0436
Лука Важник <Tyuiu.VazhnikLN@outlook.com>
2026-09-24 19:48:11 +0500

    Task5

---
+++ Tyuiu.VazhnikLN.Sprint2.Task5.V1.Lib/DataService.cs
@@ -0,0 +1,54 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.VazhnikLN.Sprint2.Task5.V1.Lib
+{
+    public class DataService : ISprint2Task5V1
+    {
+        public int FindMonthDaysCount(int value)
+        {
+            int s;
+            switch (value)
+            {
+                case 1:
+                    s = 31;
+                    break;
+                case 2:
+                    s = 28;
+                    break;
+                case 3:
+                    s = 31;
+                    break;
+                case 4:
+                    s = 30;
+                    break;
+                case 5:
+                    s = 31;
+                    break;
+                case 6:
+                    s = 30;
+                    break;
+                case 7:
+                    s = 31;
+                    break;
+                case 8:
+                    s = 31;
+                    break;
+                case 9:
+                    s = 30;
+                    break;
+                case 10:
+                    s = 31;
+                    break;
+                case 11:
+                    s = 30;
+                    break;
+                case 12:
+                    s = 31;
+                    break;
+                default:
+                    throw new ArgumentException($"Месяц должен быть от 1 до 12. Значение {value}");
+
+            }
+            return s;
+        }
+    }
+}
---
+++ Tyuiu.VazhnikLN.Sprint2.Task5.V1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Tyuiu.VazhnikLN.Sprint2.Task5.V1.Lib;
+namespace Tyuiu.VazhnikLN.Sprint2.Task5.V1
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            string res;
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите номер месяца:");
+            int x = Convert.ToInt16(Console.ReadLine());
+            if ((x >= 1) && (x <= 12)) res = "Количество дней в месяце = " + ds.FindMonthDaysCount(x);
+            else res = "Введено неверное значение!";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(res);
+
+        }
+    }
+}
