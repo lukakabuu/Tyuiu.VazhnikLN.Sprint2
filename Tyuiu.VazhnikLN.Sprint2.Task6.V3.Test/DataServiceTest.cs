c976e50beee7017e182ea137608916d7d1c5f439
Лука Важник <Tyuiu.VazhnikLN@outlook.com>
2026-09-25 07:19:09 +0500

    Task6

---
+++ Tyuiu.VazhnikLN.Sprint2.Task6.V3.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.VazhnikLN.Sprint2.Task6.V3.Lib;
+namespace Tyuiu.VazhnikLN.Sprint2.Task6.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void FindDayNameValid()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual("Понедельник", ds.FindDayName(1));
+            Assert.AreEqual("Вторник", ds.FindDayName(2));
+            Assert.AreEqual("Среда", ds.FindDayName(3));
+            Assert.AreEqual("Четверг", ds.FindDayName(4));
+            Assert.AreEqual("Пятница", ds.FindDayName(5));
+            Assert.AreEqual("Суббота", ds.FindDayName(6));
+            Assert.AreEqual("Воскресенье", ds.FindDayName(7));
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                ds.FindDayName(-1);
+            });
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                ds.FindDayName(8);
+            });
+        }
+    }
+}
---
+++ Tyuiu.VazhnikLN.Sprint2.Task6.V3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Tyuiu.VazhnikLN.Sprint2.Task6.V3.Lib;
+namespace Tyuiu.VazhnikLN.Sprint2.Task6.V3
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            string res;
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите номер дня:");
+            int x = Convert.ToInt16(Console.ReadLine());
+            if ((x >= 1) && (x <= 7)) res = "Сегодня " + ds.FindDayName(x); 
+            else res = "Введено неверное значение!";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(res);
+
+        }
+    }
+}
