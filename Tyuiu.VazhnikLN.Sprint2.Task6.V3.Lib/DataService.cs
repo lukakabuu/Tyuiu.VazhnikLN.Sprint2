fd14e614b37fb09d35fa5be6cb57b6950dbb0667
Лука Важник <Tyuiu.VazhnikLN@outlook.com>
2026-09-25 07:21:44 +0500

    Task6

--- Tyuiu.VazhnikLN.Sprint2.Task6.V3.Lib/DataService.cs
+++ Tyuiu.VazhnikLN.Sprint2.Task6.V3.Lib/DataService.cs
@@ -7,13 +7,13 @@ namespace Tyuiu.VazhnikLN.Sprint2.Task6.V3.Lib
         {
             switch (value)
             {
-                case 1: return "Понедельник";
-                case 2: return "Вторник";
-                case 3: return "Среда";
-                case 4: return "Четверг";
-                case 5: return "Пятница";
-                case 6: return "Суббота";
-                case 7: return "Воскресенье";
+                case 1: return "понедельник";
+                case 2: return "вторник";
+                case 3: return "среда";
+                case 4: return "четверг";
+                case 5: return "пятница";
+                case 6: return "суббота";
+                case 7: return "воскресенье";
                 default: throw new ArgumentException($"День должен быть от 1 до 7, значение {value}"); ;
 
             }
--- Tyuiu.VazhnikLN.Sprint2.Task6.V3.Test/DataServiceTest.cs
+++ Tyuiu.VazhnikLN.Sprint2.Task6.V3.Test/DataServiceTest.cs
@@ -8,13 +8,13 @@ namespace Tyuiu.VazhnikLN.Sprint2.Task6.V3.Test
         public void FindDayNameValid()
         {
             DataService ds = new DataService();
-            Assert.AreEqual("Понедельник", ds.FindDayName(1));
-            Assert.AreEqual("Вторник", ds.FindDayName(2));
-            Assert.AreEqual("Среда", ds.FindDayName(3));
-            Assert.AreEqual("Четверг", ds.FindDayName(4));
-            Assert.AreEqual("Пятница", ds.FindDayName(5));
-            Assert.AreEqual("Суббота", ds.FindDayName(6));
-            Assert.AreEqual("Воскресенье", ds.FindDayName(7));
+            Assert.AreEqual("понедельник", ds.FindDayName(1));
+            Assert.AreEqual("вторник", ds.FindDayName(2));
+            Assert.AreEqual("среда", ds.FindDayName(3));
+            Assert.AreEqual("четверг", ds.FindDayName(4));
+            Assert.AreEqual("пятница", ds.FindDayName(5));
+            Assert.AreEqual("суббота", ds.FindDayName(6));
+            Assert.AreEqual("воскресенье", ds.FindDayName(7));
             Assert.ThrowsException<ArgumentException>(() =>
             {
                 ds.FindDayName(-1);
